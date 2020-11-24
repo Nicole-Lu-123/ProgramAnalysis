92732f7970c320adcd8dea7ec8c77ade4b82a753
UBC Student <yuxinti@students.cs.ubc.ca>
2020-11-24 03:58:27 -0800

    Update App.java
    app

--- Project2/my-app/src/main/java/com/mycompany/app/App.java
+++ Project2/my-app/src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,54 @@
 package com.mycompany.app;
 
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitor;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    public static void main(String[] args) throws Exception {
+        File file = new File("/Users/tianyuxin/Downloads/project1/DSL-photoeditor/src/ast/Var.java");
+        System.out.println("path = " + file.getPath());
+        String FILE_PATH = file.getPath();
+        CompilationUnit cu = StaticJavaParser.parse(new FileInputStream(FILE_PATH));
+
+        VoidVisitor<?> methodNameVisitor = new App.MethodNamePrinter();
+        methodNameVisitor.visit(cu, null);
+
+        List<String> methodNames = new ArrayList<>();
+        VoidVisitor<List<String>> methodNameCollector = new App.MethodNameCollector();
+        methodNameCollector.visit(cu, methodNames);
+        methodNames.forEach(n -> System.out.println("Method Name Collected: " + n));
     }
+
+    private static class MethodNamePrinter extends VoidVisitorAdapter<Void> {
+
+        @Override
+        public void visit(MethodDeclaration md, Void arg) {
+            super.visit(md, arg);
+            System.out.println("Method Name Printed: " + md.getName());
+        }
+    }
+
+    private static class MethodNameCollector extends VoidVisitorAdapter<List<String>> {
+
+        @Override
+        public void visit(MethodDeclaration md, List<String> collector) {
+            super.visit(md, collector);
+            collector.add(md.getNameAsString());
+        }
+    }
+
 }
