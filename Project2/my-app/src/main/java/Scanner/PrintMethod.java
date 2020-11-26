3ef85848abf4852df315e320acef5ff6ffae38fb
UBC Student <yuxinti@students.cs.ubc.ca>
2020-11-25 22:18:04 -0800

    Update PrintMethod.java

--- Project2/my-app/src/main/java/Scanner/PrintMethod.java
+++ Project2/my-app/src/main/java/Scanner/PrintMethod.java
@@ -18,14 +18,14 @@ public class PrintMethod extends VoidVisitorAdapter<Void> {
         countNumMethod1++;
         System.out.println("Method used in this class: " + sn);
         System.out.println("number of for method in this class: " + countNumMethod1);
-        System.out.println("#loop: " + countStates);
+//         System.out.println("#loop: " + countStates);
 
     }
 
-    public void visit(ForStmt f, Void arg) {
-        super.visit(f, arg);
-        countStates++;
-    }
+//     public void visit(ForStmt f, Void arg) {
+//         super.visit(f, arg);
+//         countStates++;
+//     }
 
 
 
