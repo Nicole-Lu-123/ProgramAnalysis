28d3a2c6e2e0e6e58273443bd35543f17ecb0fe3
liuce <liuce9807@gmail.com>
2020-11-26 03:49:39 -0800

    Complete first commit visualization

--- Project2/my-app/src/main/java/com/mycompany/app/Main.java
+++ Project2/my-app/src/main/java/com/mycompany/app/Main.java
@@ -4,15 +4,15 @@ import Compare.Compare;
 import Dependency.Dependency;
 import Scanner.CollectClass;
 import Scanner.CollectName;
-//
+
 import Dependency.Dependency;
 import Scanner.CollectClass;
 import Scanner.CollectName;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-//import com.mycompany.BasicSimpleUniverse;
-//import com.sun.j3d.utils.applet.MainFrame;
-//import inherited.inheritedClass;
+import com.mycompany.BasicSimpleUniverse;
+import com.sun.j3d.utils.applet.MainFrame;
+import inherited.inheritedClass;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -25,29 +25,32 @@ public class Main {
 
         //File file1 = new File("C:\\cpsc410_project1_team16-master\\DSL-photoeditor\\src");
         //File file2 = new File("/Users/tianyuxin/Downloads/tinyVarsProcSkeleton-3/src");
-//        File file1 = new File("C:\\Users\\liuce\\Desktop\\cpsc410_project1_team16-master\\DSL-photoeditor\\src");
-//        CombineInfo cbi1 = new CombineInfo(file1.getPath());
+        File file1 = new File("C:\\Users\\liuce\\Desktop\\cpsc410_project1_team16-master\\DSL-photoeditor\\src");
+        CombineInfo cbi1 = new CombineInfo(file1.getPath());
         // cbi1.generate(); // delete, add this to CombineInfo constructor
 
-//        System.out.println("Done first commit analysis!!!");
+        System.out.println("Done first commit analysis!!!");
 //        for(String s :cbi1.classdependinfo.get("PhotoeditorParser")){
 //            System.out.println(s);
 //        }
 //        CombineInfo cbi2 = new CombineInfo(file1.getPath());
-        String oldpath = "C:\\Users\\卢晓萱\\Desktop\\cpsc410_project1_team16-master\\DSL-photoeditor\\src";
-        String newpath = "C:\\Users\\卢晓萱\\Desktop\\cpsc410_project1_team16-9953fa8b1a4ac0d26668bd2a1a5fb09ec5a806a7\\DSL-photoeditor\\src";
-        Compare compare = new Compare(oldpath,newpath);
-        List<String> add = compare.compareClass().get(0);
-        List<String> delet = compare.compareClass().get(1);
-
-        System.out.println("############################");
-        for(String s :delet){
-            System.out.println(s);
-        }
+//        String oldpath = "C:\\cpsc410_project1_team16-master\\DSL-photoeditor\\src\\ast";
+//        String newpath = "C:\\cpsc410_project1_team16-master\\DSL-photoeditor\\src\\libs";
+//        Compare compare = new Compare(oldpath,newpath);
+//        List<String> addclass = compare.compareclass().get(0);
+//        List<String> deletclass = compare.compareclass().get(1);
+//        System.out.println("Add class:");
+//        for(String s :addclass){
+//            System.out.println(s);
+//        }
+//        System.out.println("Delete class:");
+//        for(String s :deletclass){
+//            System.out.println(s);
+//        }
 
 
         // visualization
-//        new MainFrame(new BasicSimpleUniverse(cbi1), 700, 700);
+        new MainFrame(new BasicSimpleUniverse(cbi1), 700, 700);
     }
 
 }
