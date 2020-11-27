03a72e8c6524b42ad85e5c5eb570e3c74d630703
UBC Student <yuxinti@students.cs.ubc.ca>
2020-11-27 03:14:05 -0800

    Update SetOfName.java
    print method which contains loop

--- Project2/my-app/src/main/java/Scanner/SetOfName.java
+++ Project2/my-app/src/main/java/Scanner/SetOfName.java
@@ -6,11 +6,18 @@ import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import java.util.List;
 
 public class SetOfName extends VoidVisitorAdapter<List<String>> {
+    public List<String> MethodWhichhasLoop;
 
     @Override
     public void visit(MethodDeclaration methodDec, List<String> collection_1) {
         super.visit(methodDec, collection_1);
         String str1 = methodDec.getNameAsString();
         collection_1.add(str1);
+        CollectName cn = new CollectName();
+
+        if(cn.process(methodDec)== true) {
+//            MethodWhichhasLoop.add(methodDec.getNameAsString());
+            System.out.println("contain for loop? " + str1);
+        }
     }
 }
